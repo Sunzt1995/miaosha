b29c1c5f44b638ed444bc1eafe453c8e318b3dc6
Sunzt1995 <sunzt1995@163.com>
2020-05-29 16:32:30 +0800

    添加前端页面和sql

---
+++ miaoshao.sql
@@ -0,0 +1,217 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 192.168.25.101    Database: miaosha
+-- ------------------------------------------------------
+-- Server version	5.7.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL DEFAULT '',
+  `price` decimal(64,5) NOT NULL DEFAULT '0.00000',
+  `description` varchar(500) NOT NULL DEFAULT '',
+  `sales` int(11) NOT NULL DEFAULT '0',
+  `img_url` varchar(128) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'Iphone11',8000.00000,'最好用的苹果手机',2,'https://i.loli.net/2020/05/17/xcnvaWJliz8AjuU.jpg'),(2,'Ipad2020',2300.00000,'平板电脑',0,'https://i.loli.net/2020/05/17/vtmBxnc713UV8Mr.png');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_stock`
+--
+
+DROP TABLE IF EXISTS `item_stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stock` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_stock`
+--
+
+LOCK TABLES `item_stock` WRITE;
+/*!40000 ALTER TABLE `item_stock` DISABLE KEYS */;
+INSERT INTO `item_stock` VALUES (1,97,1),(2,200,2);
+/*!40000 ALTER TABLE `item_stock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_info`
+--
+
+DROP TABLE IF EXISTS `order_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_info` (
+  `id` varchar(32) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `item_price` decimal(64,5) NOT NULL DEFAULT '0.00000',
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `order_price` decimal(64,5) NOT NULL DEFAULT '0.00000',
+  `promo_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_info`
+--
+
+LOCK TABLES `order_info` WRITE;
+/*!40000 ALTER TABLE `order_info` DISABLE KEYS */;
+INSERT INTO `order_info` VALUES ('2020051800000000',7,1,8000.00000,1,8000.00000,0),('2020051800000100',7,1,8000.00000,1,8000.00000,0),('2020051800000200',7,1,100.00000,1,100.00000,1);
+/*!40000 ALTER TABLE `order_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `promo`
+--
+
+DROP TABLE IF EXISTS `promo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `promo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `promo_name` varchar(45) NOT NULL DEFAULT '',
+  `start_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `promo_item_price` decimal(64,5) NOT NULL DEFAULT '0.00000',
+  `end_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `promo`
+--
+
+LOCK TABLES `promo` WRITE;
+/*!40000 ALTER TABLE `promo` DISABLE KEYS */;
+INSERT INTO `promo` VALUES (1,'Iphone11抢购','2020-05-18 21:03:00',1,100.00000,'2020-05-21 12:00:00');
+/*!40000 ALTER TABLE `promo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sequence_info`
+--
+
+DROP TABLE IF EXISTS `sequence_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sequence_info` (
+  `name` varchar(255) NOT NULL,
+  `current_value` int(11) NOT NULL DEFAULT '0',
+  `step` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sequence_info`
+--
+
+LOCK TABLES `sequence_info` WRITE;
+/*!40000 ALTER TABLE `sequence_info` DISABLE KEYS */;
+INSERT INTO `sequence_info` VALUES ('order_info',3,1);
+/*!40000 ALTER TABLE `sequence_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_info`
+--
+
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL DEFAULT '',
+  `gender` tinyint(2) NOT NULL DEFAULT '0' COMMENT '// 1代表男性，2代表女性',
+  `age` int(11) NOT NULL DEFAULT '-1',
+  `telephone` varchar(45) NOT NULL DEFAULT '',
+  `register_mode` varchar(45) NOT NULL DEFAULT '' COMMENT '//byphone, bywechat, byalipay',
+  `third_party_id` varchar(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `telephone_unique_index` (`telephone`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_info`
+--
+
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+INSERT INTO `user_info` VALUES (1,'第一个用户',1,30,'13355554444','byphone',''),(7,'Tom',1,22,'13344445555','byphone','');
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_password`
+--
+
+DROP TABLE IF EXISTS `user_password`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_password` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `encrpt_password` varchar(125) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_password`
+--
+
+LOCK TABLES `user_password` WRITE;
+/*!40000 ALTER TABLE `user_password` DISABLE KEYS */;
+INSERT INTO `user_password` VALUES (1,'dsdsdfdfdsfasda',1),(3,'4QrcOUm6Wau+VuBX8g+IPg==',7);
+/*!40000 ALTER TABLE `user_password` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-29 16:28:55
